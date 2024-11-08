6b9e9cfd0d7f99914d1c62c2a86a7b15c095218f
Kerim <kerim.grbz.01@gmail.com>
2024-11-08 16:52:42 +0100

    vectors

---
+++ exercises/ouvrir_fichier.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*
+int main(){
+    string nom_fichier;
+    cout<<"entrez le  file name" <<endl;
+    getline(cin, nom_fichier);
+
+    ofstream fichier_sortie(nom_fichier, ios::app);
+
+    if(!fichier_sortie){
+        cerr<<"Erreur, impossible..."<<endl;
+        return EXIT_FAILURE;
+    }
+
+    string texte;
+    string terminer = "#exit#";
+
+    while(getline(cin, texte)){
+
+        if(texte ==terminer) break;
+        fichier_sortie<< texte<<endl;
+
+    }
+
+    fichier_sortie.close();
+
+    cout<<"Le texte a été enregistre avec succès\n";
+
+    
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}*/
+
+
---
+++ exercises/vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int index;
+    cout << "Lutfen bir index girin 0-4: ";
+    cin >> index;
+    cout << "Girilen deger: " << index << endl;
+
+    return 0;
+}
