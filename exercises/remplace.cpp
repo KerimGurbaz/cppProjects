45f844ee7c5a51f530fbe8acf3c126df87932179
Kerim <kerim.grbz.01@gmail.com>
2024-11-16 21:35:56 +0100

    modifie une texte v2

--- exercises/remplace.cpp
+++ exercises/remplace.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-string remplacer(const string& texte, const string& s_rechercher, const string& s_replacer){
+string remplacer(const string& texte, const string& s_rechercher, const string& s_remplacer){
     // Créer une copie de la chaine d'entrée pour travailler dessus
     string resultat = texte;
 
@@ -12,11 +12,24 @@ string remplacer(const string& texte, const string& s_rechercher, const string& 
     // Boucle tant que s_recherce est trouvé dans resultat
     while((pos = resultat.find(s_rechercher, pos)) != string::npos){
         // remplacer  s_rechercher par s_replacer à la position trouvée
-        resultat.replace(pos, s_rechercher.length(), s_replacer);
+        resultat.replace(pos, s_rechercher.length(), s_remplacer);
 
         // Avancer la position pour continuer la recherche après la chaine remplacée
-        pos += s_remplacer.lenght();
+        pos += s_remplacer.length();
     }
 
     return resultat;
 }
+
+
+int main(){
+
+    const string ENTREE = "Gaston   l'telefon qui son \n"
+                          "Et y a jamais persin qui y repond \n";
+
+    string sortie = remplacer(ENTREE, "on", "_on_");
+
+    cout<<sortie;
+    return 0;
+    
+    }
