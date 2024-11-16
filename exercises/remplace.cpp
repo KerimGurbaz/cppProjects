f3128c2d71bf30bbf0bff2679f95e3ca8dcd7568
Kerim <kerim.grbz.01@gmail.com>
2024-11-16 22:57:34 +0100

    modifie une texte , deuixeme fois

--- exercises/remplace.cpp
+++ exercises/remplace.cpp
@@ -1,4 +1,4 @@
-#include <iostream> 
+/*#include <iostream> 
 #include <string>
 using namespace std;
 
@@ -33,3 +33,40 @@ int main(){
     return 0;
     
     }
+*/#include <iostream>
+#include <string>
+using namespace std;
+
+string remplacer (const string& texte, const string& s_rechercher, const string& s_remplacer){
+    // 1. Créer une copie de la chaine
+
+    string resultat = texte;
+
+    //2. Initialiser la position de recherche à 0
+    size_t pos = 0;
+
+    // 3.Boucle tant que s_rechercher est trouvé dans resultat
+    while((pos = resultat.find(s_rechercher, pos)) != string::npos){
+        // Remplacer s_rechercher par s_remplacer à la position trouve
+        resultat.replace(pos, s_rechercher.length(), s_remplacer);
+
+        pos += s_remplacer.length();
+    }
+
+    return resultat;
+
+}
+
+int main()
+{
+    const string ENTREE = "Gaston y a l'telefon qui son \n"
+                          "Et y a jamais person qui y repond \n";
+
+    // Appeler la fonction 'remplacer' pour remplacer "on" par "_on_"
+    string sortie = remplacer(ENTREE, "on", "_on_");
+    
+    // Afficher le résultat
+    cout << sortie;
+
+    return 0;
+}
