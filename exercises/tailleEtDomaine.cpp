698e20ba9bc7c747000e3a124f0dbcc4cd9ee16a
Kerim <kerim.grbz.01@gmail.com>
2024-11-18 22:25:02 +0100

    declaration des pointeurs

---
+++ exercises/tailleEtDomaine.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*int main(){
+
+    float t[3][4];
+    float som = 0;
+
+    float *ptr = &t[0][0]; //pointeur pointant au premier élément
+    for(int i = 0; i<3*4 ;i++){
+        som += *(ptr + i);
+    }
+
+
+    return 0;
+}
+*/
+
